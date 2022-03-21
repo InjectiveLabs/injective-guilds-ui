e81b9fe417cce4cba945f8c582f939f89734a00a
TienPQ149 <tienpq149@gmail.com>
2022-03-22 00:28:50 +0700

    feat: adding Guild cards to home page

--- pages/index.vue
+++ pages/index.vue
@@ -13,13 +13,141 @@
         </div>
       </div>
     </section>
+    <div class="flex flex-wrap">
+      <GuildCard
+        v-for="guild in guildData"
+        :key="guild.name"
+        :guild-info="guild"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { BigNumberInBase } from '@injectivelabs/utils'
+import GuildCard from '~/components/partials/home/guild-card.vue'
 
 export default Vue.extend({
+  components: { GuildCard },
+  data() {
+    return {
+      guildData: [
+        {
+          name: 'Schneider',
+          image: '/svg/hero-1.svg',
+          memberAmount: new BigNumberInBase('13540'),
+          apy: 15,
+          totalAssetsAmount: new BigNumberInBase('1388783'),
+          assets: [
+            {
+              symbol: 'BNB',
+              logo: '/svg/icons/BNB.svg',
+              name: 'BNB'
+            },
+            {
+              symbol: 'USDT',
+              logo: '/svg/icons/USDT.svg',
+              name: 'USDT'
+            },
+            {
+              symbol: 'USDC',
+              logo: '/svg/icons/USDC.svg',
+              name: 'USDC'
+            }
+          ],
+          isFull: false,
+          isJoinable: false,
+          isJoined: true,
+          isQualified: true
+        },
+        {
+          name: 'Ethixx',
+          image: '/svg/hero-1.svg',
+          memberAmount: new BigNumberInBase('1352'),
+          apy: 10,
+          totalAssetsAmount: new BigNumberInBase('123456789'),
+          assets: [
+            {
+              symbol: 'BNB',
+              logo: '/svg/icons/BNB.svg',
+              name: 'BNB'
+            },
+            {
+              symbol: 'USDT',
+              logo: '/svg/icons/USDT.svg',
+              name: 'USDT'
+            },
+            {
+              symbol: 'USDC',
+              logo: '/svg/icons/USDC.svg',
+              name: 'USDC'
+            }
+          ],
+          isFull: false,
+          isJoinable: true,
+          isJoined: false,
+          isQualified: true
+        },
+        {
+          name: 'Akukx',
+          image: '/svg/hero-1.svg',
+          memberAmount: new BigNumberInBase('1552'),
+          apy: 10,
+          totalAssetsAmount: new BigNumberInBase('1234789'),
+          assets: [
+            {
+              symbol: 'BNB',
+              logo: '/svg/icons/BNB.svg',
+              name: 'BNB'
+            },
+            {
+              symbol: 'USDT',
+              logo: '/svg/icons/USDT.svg',
+              name: 'USDT'
+            },
+            {
+              symbol: 'USDC',
+              logo: '/svg/icons/USDC.svg',
+              name: 'USDC'
+            }
+          ],
+          isFull: true,
+          isJoinable: false,
+          isJoined: false,
+          isQualified: true
+        },
+        {
+          name: 'Asepoid',
+          image: '/svg/hero-1.svg',
+          memberAmount: new BigNumberInBase('1452'),
+          apy: 12,
+          totalAssetsAmount: new BigNumberInBase('126789'),
+          assets: [
+            {
+              symbol: 'BNB',
+              logo: '/svg/icons/BNB.svg',
+              name: 'BNB'
+            },
+            {
+              symbol: 'USDT',
+              logo: '/svg/icons/USDT.svg',
+              name: 'USDT'
+            },
+            {
+              symbol: 'USDC',
+              logo: '/svg/icons/USDC.svg',
+              name: 'USDC'
+            }
+          ],
+          isFull: false,
+          isJoinable: false,
+          isJoined: false,
+          isQualified: false
+        }
+      ]
+    }
+  },
   methods: {
     logout() {
       this.$accessor.wallet.logout()
